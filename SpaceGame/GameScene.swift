214d8fd2aaadd7dc5300d4c265c56f1e776ee53b
Nikolai Saganenko <9952728@gmail.com>
2021-11-14 14:23:00 +0300

    Ready to play

--- SpaceGame/GameScene.swift
+++ SpaceGame/GameScene.swift
@@ -11,21 +11,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var starfield: SKEmitterNode!
     var player: SKSpriteNode!
     var scoreLabel: SKLabelNode!
-    let playAgain: SKLabelNode!
+    var playAgain: SKLabelNode!
     
     var possibleEnemies = ["ball", "hammer", "tv"]
     var gameTimer: Timer?
-    var isGameOver = false {
-        didSet{
-            gameTimer?.invalidate()
-            playAgain = SKLabelNode(fontNamed: "Chalkduster")
-            playAgain.position = CGPoint(x: 512, y: 384)
-            playAgain.horizontalAlignmentMode = .center
-            playAgain.fontSize = 54
-            playAgain.text = "Play again"
-            addChild(playAgain)
-        }
-    }
+    var isGameOver = false
     var timeInterval = 1.0
     var enemiesCreatedNumber = 0
 
@@ -40,8 +30,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         starfield = SKEmitterNode(fileNamed: "starfield")
         starfield.position = CGPoint(x: 1024, y: 384)
         starfield.advanceSimulationTime(10)
+        starfield.name = "bg"
         addChild(starfield)
         starfield.zPosition = -1
+        
+        physicsWorld.gravity = .zero
+        physicsWorld.contactDelegate = self // tell us when contact happen
+        
+        startGame()
+    }
+    
+    func startGame() {
+        
+        if scoreLabel != nil{
+            scoreLabel.removeFromParent()
+        }
         
         player = SKSpriteNode(imageNamed: "player")
         player.position = CGPoint(x: 100, y: 384)
@@ -52,14 +55,32 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
         scoreLabel.position = CGPoint(x: 16, y: 16)
         scoreLabel.horizontalAlignmentMode = .left
+        scoreLabel.name = "score"
         addChild(scoreLabel)
         
         score = 0
-        
-        physicsWorld.gravity = .zero
-        physicsWorld.contactDelegate = self // tell us when contact happen
+        timeInterval = 1.0
+        enemiesCreatedNumber = 0
+        isGameOver = false
         
         gameTimer = Timer.scheduledTimer(timeInterval: timeInterval, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+    }
+    
+    func finishGame() {
+        for child in self.children {
+            if child.name == "bg" || child.name == "score" {
+                continue
+            }
+            child.removeFromParent()
+        }
+        
+        playAgain = SKLabelNode(fontNamed: "Chalkduster")
+        playAgain.position = CGPoint(x: 512, y: 384)
+        playAgain.horizontalAlignmentMode = .center
+        playAgain.fontSize = 54
+        playAgain.name = "playAgain"
+        playAgain.text = "Play again"
+        addChild(playAgain)
     }
 
     @objc func createEnemy() {
@@ -105,6 +126,28 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
         
         player.position = location
+        
+        // Make a little delay
+//        let moveToFinger = SKAction.move(to: location, duration: 0.3)
+//        player.run(moveToFinger)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let moveToBase = SKAction.move(to: CGPoint(x: 50, y: 384), duration: 0.3)
+        player.run(moveToBase)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {return}
+        let location = touch.location(in: self)
+        let objects = nodes(at: location)
+        
+        if playAgain != nil {
+            if objects.contains(playAgain) {
+                playAgain.removeFromParent()
+                startGame()
+            }
+        }
     }
     
     // if contact happens
@@ -112,8 +155,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let explosion = SKEmitterNode(fileNamed: "explosion")!
         explosion.position = player.position
         addChild(explosion)
-        
         player.removeFromParent()
         isGameOver = true
+        gameTimer?.invalidate()
+        
+        let wait = SKAction.wait(forDuration: 1)
+        let finish = SKAction.run {self.finishGame()}
+        let sequence = SKAction.sequence([wait, finish])
+        run(sequence)
     }
 }
